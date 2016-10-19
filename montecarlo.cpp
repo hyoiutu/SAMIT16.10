295e77e6e65b4dc38910168f1af18b8aca08c130
hyoiutu <ke-suke.flandre@ezweb.ne.jp>
2016-10-19 21:26:36 +0900

    あらかた完成した．後は手直しとソースコード作成

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 inline double f(double x){return sqrt(1 - x*x);};
 double montecarlo(double begin, double end, int numOfPlot);
+double quasiMontecarlo(double begin, double end, int numOfPlot);
+
+double vandercorput(int b, int n,int i=0);
 
 int main(void)
 {
-  cout << "S=" << 4*montecarlo(0.0,1.0,10000000) << endl;
+  cout << "モンテカルロ法:S=" << 4*montecarlo(0.0,1.0,10000000) << endl;
+  cout << "準モンテカルロ法:S=" << 4*quasiMontecarlo(0.0,1.0,10000000) << endl;
+
 }
 
 double montecarlo(double begin, double end, int numOfPlot)
@@ -28,4 +33,30 @@ double montecarlo(double begin, double end, int numOfPlot)
     }
   }
   return Sa * inX / numOfPlot;
+}
+
+double quasiMontecarlo(double begin, double end, int numOfPlot)
+{
+  double Sa = (end-begin) * (end-begin);
+  int inX = 0;
+  for(int i=0;i<numOfPlot;++i)
+  {
+    double randTmpX = begin + end*vandercorput(2,i);
+    double randTmpY = begin + end*vandercorput(3,i);
+    if(f(randTmpX) > randTmpY)
+    {
+      inX++;
+    }
+  }
+  return Sa * inX / numOfPlot;
+}
+
+
+double vandercorput(int b, int n, int i)
+{
+  if(n == 0)
+  {
+    return 0;
+  }
+  return (n%b)/pow(b,i+1) + vandercorput(b, n/b, ++i);
 }
